c7aa5fb56b3151d49cc9431ebe7d07cad4292678
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2024-11-13 20:06:59 +0900

    fmt,lint

--- tmplot/twin/plot.py
+++ tmplot/twin/plot.py
@@ -6,12 +6,6 @@ from .common import CommonPlotter
 @dataclass
 class Plot(CommonPlotter):
     def run(self) -> None:
-        self.ax2.plot(
-            self.data[:, 0],
-            self.data[:, 1]
-        )
-        self.ax2.plot(
-            self.data[:, 0],
-            self.data[:, 2]
-        )
+        self.ax2.plot(self.data[:, 0], self.data[:, 1])
+        self.ax2.plot(self.data[:, 0], self.data[:, 2])
         self.save()
--- tmplot/twin/scatter.py
+++ tmplot/twin/scatter.py
@@ -6,12 +6,6 @@ from .common import CommonPlotter
 @dataclass
 class Scatter(CommonPlotter):
     def run(self) -> None:
-        self.ax2.scatter(
-            self.data[:, 0],
-            self.data[:, 1]
-        )
-        self.ax2.scatter(
-            self.data[:, 0],
-            self.data[:, 2]
-        )
+        self.ax2.scatter(self.data[:, 0], self.data[:, 1])
+        self.ax2.scatter(self.data[:, 0], self.data[:, 2])
         self.save()
