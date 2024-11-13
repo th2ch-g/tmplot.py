a597192a7d84f6000d54bcd19464d5d69df37ba5
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2024-11-13 20:36:05 +0900

    pipenv to uv

--- tmplot/logger.py
+++ tmplot/logger.py
@@ -3,7 +3,8 @@ from logging import DEBUG, Formatter, Logger, StreamHandler, getLogger
 
 def generate_logger(logger_name: str) -> Logger:
     logger = getLogger(logger_name)
-    logfmt = "%(levelname)-9s  %(asctime)s  [%(filename)s.%(funcName)s.%(lineno)d] %(message)s"
+    logfmt = "%(levelname)-9s %(asctime)s \
+            [%(filename)s.%(funcName)s.%(lineno)d] %(message)s"
     datefmt = "%Y-%m-%d %H:%M:%S"
     stream_handler = StreamHandler()
     stream_handler.setLevel(DEBUG)
