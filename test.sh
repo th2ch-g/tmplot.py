8fa6bd5e97e216ebc5b97eaec1c685b0e2d9f5b7
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2024-11-14 09:23:17 +0900

    chore

--- test.sh
+++ test.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-cat data/sample.txt | uv run python3 -m tmplot plot -f - -o test1.png
-cat data/sample.txt | uv run python3 -m tmplot scatter -f - -o test2.png
-cat data/sample.txt | awk '{print $2}' | uv run python3 -m tmplot hist -f - -o test3.png
-cat data/sample.xvg | uv run python3 -m tmplot plot -f - -o test1_2.png
+tmplot="uv run python3 -m tmplot"
 
-uv run python3 -m tmplot cat plot -f data/sample.txt data/sample.xvg -o test4.png
-uv run python3 -m tmplot cat scatter -f data/sample.txt data/sample.xvg -o test5.png
-uv run python3 -m tmplot cat hist -f data/sample.txt data/sample.xvg -o test6.png
+cat data/sample.txt | $tmplot plot -f - -o test1.png
+cat data/sample.txt | $tmplot scatter -f - -o test2.png
+cat data/sample.txt | awk '{print $2}' | $tmplot hist -f - -o test3.png
+cat data/sample.xvg | $tmplot plot -f - -o test1_2.png
 
-cat data/sample2.txt | uv run python3 -m tmplot twin plot -f - -o test7.png
-cat data/sample2.txt | uv run python3 -m tmplot twin scatter -f - -o test8.png
+$tmplot cat plot -f data/sample.txt data/sample.xvg -o test4.png
+$tmplot cat scatter -f data/sample.txt data/sample.xvg -o test5.png
+$tmplot cat hist -f data/sample.txt data/sample.xvg -o test6.png
+
+cat data/sample2.txt | $tmplot twin plot -f - -o test7.png
+cat data/sample2.txt | $tmplot twin scatter -f - -o test8.png
